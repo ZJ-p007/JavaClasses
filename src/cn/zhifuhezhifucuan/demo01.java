6f9f7e804bf12d4adbb5a12dda95cd53876320a3
ZJ-p007 <zj636500@qq.com>
2020-10-26 19:53:48 +0800

    字符和字符串参与加法运算

---
+++ src/cn/zhifuhezhifucuan/demo01.java
@@ -0,0 +1,19 @@
+package cn.zhifuhezhifucuan;
+
+public class demo01 {
+    public static void main(String[] args) {
+        //字符的加法运算
+        //定义两个变量
+        int a = 10;
+        char ch = 'a';//97
+        System.out.println(a + ch);
+
+        //字符串的加法运算
+        System.out.println("hello" + "world");//helloworld
+        System.out.println("hello" + 10);//hello10
+        System.out.println("hello" + 10 + 20);//hello1020
+        System.out.println(10 + 20 + "hello");//30hello
+
+    }
+
+}
